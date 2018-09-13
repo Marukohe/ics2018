9e761271d0347c5ead8ac7dc6b181d51f670af88
tracer-ics2018 <tracer@njuics.org>
2018-09-13 02:24:26 +0000

    >  compile 171240537 hw Linux 1ddb05a31ba2 4.9.93-linuxkit-aufs #1 SMP Wed Jun 6 16:55:56 UTC 2018 x86_64 GNU/Linux  02:24:26 up  1:09,  4 users,  load average: 0.02, 0.01, 0.00 f7290eb840a109589657b4b0f5d9ad05348097e6

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -75,14 +75,14 @@ static int cmd_si(char *args){
 static int cmd_info(char *args)
 {
 	 if(strcmp(args,"r")==0){
-			printf("eax\t%-x\t%-d\n",cpu.eax,cpu.eax);
-			printf("ecx\t%-x\t%-d\n",cpu.ecx,cpu.ecx);
-			printf("edx\t%-x\t%-d\n",cpu.edx,cpu.edx);
-			printf("ebx\t%-x\t%-d\n",cpu.ebx,cpu.ebx);
-			printf("esp\t%-x\t%-d\n",cpu.esp,cpu.esp);
-			printf("ebp\t%-x\t%-d\n",cpu.ebp,cpu.ebp);
-			printf("esi\t%-x\t%-d\n",cpu.esi,cpu.esi);
-			printf("edi\t%-x\t%-d\n",cpu.edi,cpu.edi);
+			printf("eax\t%-16x%-d\n",cpu.eax,cpu.eax);
+			printf("ecx\t%-16x%-d\n",cpu.ecx,cpu.ecx);
+			printf("edx\t%-16x%-d\n",cpu.edx,cpu.edx);
+			printf("ebx\t%-16x%-d\n",cpu.ebx,cpu.ebx);
+			printf("esp\t%-16x%-d\n",cpu.esp,cpu.esp);
+			printf("ebp\t%-16x%-d\n",cpu.ebp,cpu.ebp);
+			printf("esi\t%-16x%-d\n",cpu.esi,cpu.esi);
+			printf("edi\t%-16x%-d\n",cpu.edi,cpu.edi);
 			return 0;
 	 }
 	 else{
