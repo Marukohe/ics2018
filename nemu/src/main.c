c8714735e44fa83b2542017f01e6f6d134d3f7bb
tracer-ics2018 <tracer@njuics.org>
2018-09-26 21:01:49 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u4 (2018-08-21) x86_64 GNU/Linux  21:01:49 up  3:24,  5 users,  load average: 0.07, 0.10, 0.08 a09f9732a36983eb961b9c44506cef31d26baf60

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,8 +1,25 @@
+#include "monitor/expr.h"
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
+  uint32_t expr(char *e, bool *success);
+  FILE *fp;
+  fp=fopen("./tools/gen-expr/input","w+");
+  bool success =true;
+  uint32_t cnt;
+  while(~fscanf(fp,"%u",&cnt)){
+	 char str1[65536];
+	 fscanf(fp,"%[^n]%*c",str1);
+     uint32_t tmp =  expr(str1, &success);
+	 if(cnt==tmp)
+		 printf("true\n");
+	 else
+		 printf("ying ying ying\n");
+  }
+  fclose(fp);
   /* Initialize the monitor. */
+  	
   int is_batch_mode = init_monitor(argc, argv);
 
   /* Receive commands from user. */
