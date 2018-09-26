fdad13b005c8824f5543dc5bfe76f5d365b984c7
tracer-ics2018 <tracer@njuics.org>
2018-09-26 19:22:30 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u4 (2018-08-21) x86_64 GNU/Linux  19:22:30 up  2:00,  4 users,  load average: 0.13, 0.16, 0.09 8c9f81a249e6af05d24817c61004ddd2cedd83f3

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -264,7 +264,14 @@ uint32_t eval(int p,int q)
 			case '+': return val1+val2;
 			case '-': return val1-val2;
 			case '*': return val1*val2;
-			case '/': return val1/val2;
+			case '/':
+					  if(val2==0)
+					  {
+						  printf("divide by 0\n");
+						  return 0;
+					  }
+					  else
+						 return val1/val2;
 			default: assert(0);
 		}
 	}
