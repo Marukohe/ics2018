65c4d4293b44c10f762cee6b1c1ffba04a4bdb7b
tracer-ics2018 <tracer@njuics.org>
2018-10-04 20:00:56 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u5 (2018-09-30) x86_64 GNU/Linux  20:00:56 up  9:46,  9 users,  load average: 0.26, 0.15, 0.10 5138fe1a169ba0f9d561bfe5be3f05d0629f02f0

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,7 +7,8 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  rtl_sub(&id_dest->val,&id_dest->val,&id_src->val);
+  cpu.esp-=4;
+  /*rtl_sub(&id_dest->val,&id_dest->val,&id_src->val);*/
   /*rtl_update_ZFSF(&id_dest->val,id_dest->width);*/
 
   print_asm_template2(sub);
