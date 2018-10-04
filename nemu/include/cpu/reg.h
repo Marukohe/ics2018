a875ff45416e95fcb02578b4510eff6275c73f2a
tracer-ics2018 <tracer@njuics.org>
2018-10-04 14:55:34 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u5 (2018-09-30) x86_64 GNU/Linux  14:55:34 up  5:19,  5 users,  load average: 0.28, 0.19, 0.10 132933109fac16d890716c252b6c3e062ded3ec9

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -30,6 +30,26 @@ typedef union {
   rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 
   vaddr_t eip;
+
+  struct{
+	  uint32_t CF :1;
+	  uint32_t nothing :1;
+	  uint32_t PF :1;
+	  uint32_t    :0;
+	  uint32_t AF :1;
+	  uint32_t    :0;
+	  uint32_t ZF :1;
+	  uint32_t SF :1;
+	  uint32_t TF :1;
+	  uint32_t IF :1;
+	  uint32_t DF :1;
+	  uint32_t OF :1;
+	  uint32_t PL :1;
+	  uint32_t NT :1;
+	  uint32_t    :0;
+	  uint32_t RF :1;
+	  uint32_t VM :1;
+  }EFLAGS;
   };
 
 } CPU_state;
