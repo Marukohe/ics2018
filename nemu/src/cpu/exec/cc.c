ea2f45908665d7795ed848329f7e1d6d79d7fe7a
tracer-ics2018 <tracer@njuics.org>
2018-10-12 22:07:54 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u5 (2018-09-30) x86_64 GNU/Linux  22:07:54 up  1:43,  9 users,  load average: 0.00, 0.02, 0.00 8bab4db668524153bceb73f17c65020d0b5bca7

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -15,14 +15,47 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
+		if(cpu.EFLAGS.OF==1)
+			rtl_li(dest,1);
+		else
+			rtl_li(dest,0);
+		break;
     case CC_B:
+		if(cpu.EFLAGS.CF==1)
+			rtl_li(dest,1);
+		else
+			rtl_li(dest,0);
+		break;
     case CC_E:
+		if(cpu.EFLAGS.ZF==1)
+			rtl_li(dest,1);
+		else
+			rtl_li(dest,0);
+		break;
     case CC_BE:
+		if(cpu.EFLAGS.CF==1||cpu.EFLAGS.ZF==1)
+			rtl_li(dest,1);
+		else
+			rtl_li(dest,0);
+		break;
     case CC_S:
+		if(cpu.EFLAGS.SF==1)
+			rtl_li(dest,1);
+		else
+			rtl_li(dest,0);
+		break;
     case CC_L:
+		if(cpu.EFLAGS.SF!=cpu.EFLAGS.OF)
+			rtl_li(dest,1);
+		else
+			rtl_li(dest,0);
+		break;
     case CC_LE:
-	  rtl_li(dest,1);
-	  break;
+		if(cpu.EFLAGS.ZF==1&&cpu.EFLAGS.SF!=cpu.EFLAGS.OF)
+			rtl_li(dest,1);
+		else
+			rtl_li(dest,0);
+	    break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
