6a07aca35b781c2295fc4a23fbe0732e3d17340a
tracer-ics2018 <tracer@njuics.org>
2018-10-12 23:11:33 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u5 (2018-09-30) x86_64 GNU/Linux  23:11:33 up  2:22,  9 users,  load average: 0.00, 0.03, 0.05 f5212b40e751284c4f1f6c4e7c491fb1a92dab7b

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -164,7 +164,7 @@ opcode_entry opcode_table [512] = {
   /* 0x58 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x5c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x60 */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x64 */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x64 */	EMPTY, EMPTY,IDEXW(mov_E2G,movzx,1) , EMPTY,
   /* 0x68 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x6c */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x70 */	EMPTY, EMPTY, EMPTY, EMPTY,
