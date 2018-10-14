96b57694e2fe8c6979f489dd22886cf60e74e230
tracer-ics2018 <tracer@njuics.org>
2018-10-14 16:37:28 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u5 (2018-09-30) x86_64 GNU/Linux  16:37:28 up  7:06,  8 users,  load average: 0.00, 0.01, 0.00 515f25bf2743d7eb29e0724cecd63e71e37348cd

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -9,7 +9,7 @@ make_EHelper(test) {
 
 make_EHelper(and) {
   /*TODO();*/
-  rtl_sext(&t1,&id_src->val,4);
+  rtl_sext(&t1,&id_src->val,id_src->width);
   rtl_and(&t2,&id_dest->val,&t1);
   operand_write(id_dest,&t2);
 
