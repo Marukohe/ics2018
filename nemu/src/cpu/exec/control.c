00540fb3f28fa2a67549ade048577494dec80c72
tracer-ics2018 <tracer@njuics.org>
2018-10-15 00:36:54 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u5 (2018-09-30) x86_64 GNU/Linux  00:36:54 up 12:49, 11 users,  load average: 0.01, 0.01, 0.00 2c8b3856d0bc1132831c55bee8eb1d4f11137049

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -1,14 +1,14 @@
 #include "cpu/exec.h"
 #include "cpu/cc.h"
 
-make_EHelper(jmp) {
+make_EHelper(jmp) { 
   // the target address is calculated at the decode stage
   rtl_j(decoding.jmp_eip);
 
   print_asm("jmp %x", decoding.jmp_eip);
 }
 
-make_EHelper(jcc) {
+make_EHelper(jcc) { 
   // the target address is calculated at the decode stage
   uint32_t cc = decoding.opcode & 0xf;
   rtl_setcc(&t0, cc);
@@ -22,11 +22,12 @@ make_EHelper(jmp_rm) {
   rtl_jr(&id_dest->val);
 
   print_asm("jmp *%s", id_dest->str);
-}
+} 
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  rtl_push(&decoding.seq_eip);
+  //rtl_push(&decoding.seq_eip);
+  rtl_push(&cpu.eip);
   /*cpu.eip=id_dest->simm+decoding.seq_eip;*/
   rtl_j(decoding.jmp_eip);
  /* if(cpu.eip==0x10000a)
@@ -39,6 +40,7 @@ make_EHelper(call) {
 }
 
 make_EHelper(ret) {
+  //rtl_pop(&decoding.seq_eip);
   //rtl_pop(&decoding.seq_eip);
   rtl_pop(&cpu.eip);
   print_asm("ret");
