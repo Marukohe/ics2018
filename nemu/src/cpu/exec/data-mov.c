71a3d9631b147636d81fbb079eab1b3cd7360db0
tracer-ics2018 <tracer@njuics.org>
2018-10-16 02:51:05 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  02:51:05 up  3:42, 11 users,  load average: 0.00, 0.00, 0.00 4ec1792f99e7d6a8fca6b71c94ae1066dc87ef97

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -82,6 +82,7 @@ make_EHelper(cwtl) {
 
 make_EHelper(movsx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
+  printf("%d\n",id_src->width);
   rtl_sext(&t0, &id_src->val, id_src->width);
   operand_write(id_dest, &t0);
   print_asm_template2(movsx);
