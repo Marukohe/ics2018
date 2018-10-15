df28ea92461a45f83000fead3ea89486fd962bf6
tracer-ics2018 <tracer@njuics.org>
2018-10-16 03:49:48 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  03:49:48 up  4:41, 11 users,  load average: 0.00, 0.00, 0.00 9ae9209ba37d520936b710f93ab96c1cfe069e43

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -61,8 +61,8 @@ make_group(gp4,
 
   /* 0xff */
 make_group(gp5,
-    EX(inc), EX(dec), EX(call_rm), EMPTY,
-    EMPTY, EMPTY, EX(push), EMPTY)
+    EX(inc), EX(dec), EMPTY, EX(call_rm),
+    EMPTY, EX(jmp_rm), EX(push), EMPTY)
 
   /* 0x0f 0x01*/
 make_group(gp7,
