121d6660d417b42e3083d6ab68c84de1baf015e6
tracer-ics2018 <tracer@njuics.org>
2018-10-17 22:09:08 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  22:09:08 up 15:02, 14 users,  load average: 0.02, 0.02, 0.00 866e58f9633f555e5473e50da936a4a764183446

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -133,9 +133,9 @@ int memcmp(const void* s1, const void* s2, size_t n){
 	if(n<=0)
 		return 0;
 	if(*dst>*src)
-		return 1;
-	else if(*dst<*src)
 		return 0;
+	else if(*dst<*src)
+		return -1;
 	return 0;
 }
 
