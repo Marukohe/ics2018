79cd92df4a3293525dd205eda8d95d075d2fb607
tracer-ics2018 <tracer@njuics.org>
2018-10-17 18:25:27 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  18:25:27 up 11:29, 11 users,  load average: 0.00, 0.02, 0.00 20f91f6ad8ec982cb32acaa47bdf43c23d60f213

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -217,7 +217,7 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   //TODO();
   //rtl_li(&at,width*8-1);
   //rtl_shr(dest,src1,&at);
-  *dest=(*src1>>(width*8-1)==1);
+  *dest=((*src1>>(width*8-1))==1);
 }
 
 #define make_rtl_setget_eflags(f) \
