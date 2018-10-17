058543d39a9ab3af53a8fd7d58913e6c0a120109
tracer-ics2018 <tracer@njuics.org>
2018-10-17 19:29:18 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  19:29:18 up 12:33, 12 users,  load average: 0.09, 0.03, 0.01 d1a067fbbcc8ae33aa92d3f87bbf0aae3f7aefe

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -51,11 +51,13 @@ make_EHelper(cmp) {
   /*TODO();*/
   Log("%x %x %d",id_dest->val,id_src->val,id_dest->width);
   rtl_sub(&t2, &id_dest->val, &id_src->val);
+  Log("%x",t2);
   rtl_setrelop(RELOP_LTU, &t3, &id_dest->val, &t2);
-  
+  Log("%x",t2);
   at=id_dest->val;
+  Log("id_dest->val: %x id_src->val: %x",id_dest->val,id_src->val);
   operand_write(id_dest, &t2);
-  Log("%x",id_dest->val);
+  Log("id_dest->val: %x id_src->val: %x",id_dest->val,id_src->val);
   //at=t2;
   rtl_update_ZFSF(&t2, id_dest->width);
 
