51f50adef3f909447a85450ba72e61a11cf02302
tracer-ics2018 <tracer@njuics.org>
2018-10-30 11:29:26 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  11:29:26 up 17:08, 15 users,  load average: 0.18, 0.27, 0.27 b87142ca19e617df4f7a761c139151b085b3640c

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -170,8 +170,8 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 			*dest=*src1;
 		}
 		else{
-			rtl_li(&t2,0xffffff00);
-			rtl_or(dest,src1,&t2);
+			*dest=*src1|0xffffff00;
+			//rtl_or(dest,src1,&t2);
 		}
 		return;
 	case 2:
@@ -179,8 +179,9 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 			*dest=*src1;
 		}
 		else{
-			rtl_li(&t2,0xffff0000);
-			rtl_or(dest,src1,&t2);
+			*dest=*src1|0xffff0000;
+			//rtl_li(&t2,0xffff0000);
+			//rtl_or(dest,src1,&t2);
 		} 
 	    return;
 	case 4:
