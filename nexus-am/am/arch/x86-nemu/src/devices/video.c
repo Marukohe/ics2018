315b52526d060cc6ebfa946bb8a01764e132b708
tracer-ics2018 <tracer@njuics.org>
2018-10-30 16:32:30 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  16:32:30 up 20:34, 12 users,  load average: 0.25, 0.14, 0.11 4a7302f0783d1b33dc6fffc43f11ee17d9ee686c

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -24,6 +24,10 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
 
+	  int i;
+	  int size = screen_width() * screen_height();
+	  for(i=0;i<size;i++) fb[i]=i;
+
       if (ctl->sync) {
         // do nothing, hardware syncs.
       }
