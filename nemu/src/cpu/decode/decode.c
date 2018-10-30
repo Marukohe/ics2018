a513a91d20a5411da9d1276c7250e96b2d8dd193
tracer-ics2018 <tracer@njuics.org>
2018-10-30 11:13:58 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  11:13:58 up 16:53, 14 users,  load average: 0.12, 0.17, 0.25 860c37a5bca8b4a9782dec1d9a49d5187664ebb6

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -42,10 +42,10 @@ static inline make_DopHelper(SI) {
    *
    */
   at = instr_fetch(eip,op->width);
-  rtl_sext(&t2,&at,op->width);
+  rtl_sext(&t0,&at,op->width);
  // rtl_sext(&op->simm,&at,op->width);
   //printf("at: %d op->width: %d\n",at,op->width);
-  op->simm=t2;
+  op->simm=t0;
   rtl_li(&op->val, op->simm);
 
 #ifdef DEBUG
