9e2d0e0663b3a7f7ca1640e237e7b62a56be3a27
tracer-ics2018 <tracer@njuics.org>
2018-11-06 21:07:46 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  21:07:46 up 1 day,  9:14, 14 users,  load average: 0.20, 0.16, 0.10 4227099131cd72bee173db8feaee71948e72c61

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,25 +7,25 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    */
 
  // TODO();
-    union{
+   /* union{
 		GateDesc Gatedesc;
 		uint32_t val[2];
-	}GD;
+	}GD;*/
 
     /*push eflags*/
-	//rtl_push(&cpu.eflags);
+	rtl_push(&cpu.eflags);
 
 	/*push CS*/
-	//rtl_push(&cpu.CS);
+	rtl_push(&cpu.CS);
 
 	/*push eip*/
-	//rtl_push(&ret_addr);
+	rtl_push(&ret_addr);
 
-	GD.val[0]=vaddr_read(cpu.IDTR.base+NO*8,4);
-	GD.val[1]=vaddr_read(cpu.IDTR.base+NO*8+4,4);
+	//GD.val[0]=vaddr_read(cpu.IDTR.base+NO*8,4);
+	//GD.val[1]=vaddr_read(cpu.IDTR.base+NO*8+4,4);
 	//TODO();
-	decoding.seq_eip=GD.Gatedesc.offset_15_0+(GD.Gatedesc.offset_31_16<<16);
-
+	//decoding.seq_eip=GD.Gatedesc.offset_15_0+(GD.Gatedesc.offset_31_16<<16);
+	decoding.seq_eip=0x100b18;
 
 }
 
