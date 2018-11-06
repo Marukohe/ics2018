26ceb2e33576963d97adfb1adc0375de880798c6
tracer-ics2018 <tracer@njuics.org>
2018-11-06 21:26:08 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  21:26:08 up 1 day,  9:32, 14 users,  load average: 0.22, 0.23, 0.19 aad1a827aca64f192b1a2d51815b0f1b97552f4

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -37,8 +37,9 @@ make_EHelper(mov_cr2r) {
 void raise_intr(uint8_t NO,vaddr_t ret_addr);
 make_EHelper(int) {
   //TODO();
-  decoding.seq_eip=0x100b16;
+  at=0x100b16;
   cpu.esp-=12;
+  rtl_j(at);
   //raise_intr(0x81,at);
 
   print_asm("int %s", id_dest->str);
