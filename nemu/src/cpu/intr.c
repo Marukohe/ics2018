3e26a4eb10892453fe9cbb13375edcecec6dc054
tracer-ics2018 <tracer@njuics.org>
2018-11-06 15:41:59 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  15:41:59 up 1 day,  6:25, 11 users,  load average: 0.08, 0.07, 0.02 7f5b358b31ce6803140f517a490860b8dec7112f

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,26 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+ // TODO();
+    union{
+		GateDesc Gatedesc;
+		uint32_t val[2];
+	}GD;
+
+    /*push eflags*/
+	rtl_push(&cpu.eflags);
+
+	/*push CS*/
+	rtl_push(&cpu.CS);
+
+	/*push eip*/
+	rtl_push(&ret_addr);
+
+	GD.val[0]=vaddr_read(cpu.IDTR.base+NO*8,4);
+	GD.val[1]=vaddr_read(cpu.IDTR.base+NO*8+4,4);
+	decoding.seq_eip=GD.Gatedesc.offset_15_0+(GD.Gatedesc.offset_31_16<<16);
+
+
 }
 
 void dev_raise_intr() {
