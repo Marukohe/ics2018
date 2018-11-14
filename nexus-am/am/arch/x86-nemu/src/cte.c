70f28d820598891dbfd50ae7af5bebe9574c2bb0
tracer-ics2018 <tracer@njuics.org>
2018-11-14 10:43:05 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  10:43:05 up  7:34,  5 users,  load average: 0.27, 0.10, 0.08 d8623f0889ab027dd57fc6ae2a0d804c29e66edc

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -50,7 +50,8 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
 
   // -------------------- system call --------------------------
   idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_KERN);
-
+  //mytodo
+  idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_KERN);
   set_idt(idt, sizeof(idt));
 
   // register event handler
