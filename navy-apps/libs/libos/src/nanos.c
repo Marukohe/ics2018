4c3840ad4b060e0eee327859adc7e0c1f049dad1
tracer-ics2018 <tracer@njuics.org>
2018-11-14 17:17:01 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  17:17:01 up 12:24, 10 users,  load average: 0.06, 0.13, 0.09 1abbea27b1bd9ab55d254de522361f861c4f20ed

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -34,7 +34,8 @@ int _open(const char *path, int flags, mode_t mode) {
 
 int _write(int fd, void *buf, size_t count){
   _syscall_(SYS_write,fd,(intptr_t)buf,count);  //mytodo
-  _exit(SYS_write);
+  //_exit(SYS_write);
+  _exit(13);
   return 0;
 }
 
