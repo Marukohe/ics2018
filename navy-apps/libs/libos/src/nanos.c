3433f6c4ae3d027d6cf5e4b192c98224b979a08e
tracer-ics2018 <tracer@njuics.org>
2018-11-18 15:08:45 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  15:08:45 up 21:46,  9 users,  load average: 0.37, 0.23, 0.19 840e7bceb798c64fd4dddc12ea5126125ea2c30

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -59,9 +59,8 @@ int _close(int fd) {
 }
 
 off_t _lseek(int fd, off_t offset, int whence) {
-  _syscall_(SYS_lseek,fd,offset,whence);
+  return _syscall_(SYS_lseek,fd,offset,whence);
   //_exit(SYS_lseek);
-  return 0;
 }
 
 int _execve(const char *fname, char * const argv[], char *const envp[]) {
