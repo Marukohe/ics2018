9c056286e8450a3d2b2f6abbff7a9561f192a8ab
tracer-ics2018 <tracer@njuics.org>
2018-11-19 00:44:19 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  00:44:19 up 1 day,  4:35, 11 users,  load average: 0.67, 0.86, 0.80 d8183732d35e5496a3c983cb17a602d87563e6a6

--- navy-apps/libs/libndl/src/bmp.c
+++ navy-apps/libs/libndl/src/bmp.c
@@ -39,9 +39,9 @@ int NDL_LoadBitmap(NDL_Bitmap *bmp, const char *filename) {
 
   //assert(0);
   w = hdr.width; h = hdr.height;
-  assert(0);
+  //assert(0);
   int line_off = (w * 3 + 3) & ~0x3;
-
+  assset(0);
   for (int i = 0; i < h; i ++) {
     fseek(fp, hdr.offset + (h - 1 - i) * line_off, SEEK_SET);
     int nread = fread(&pixels[w * i], 3, w, fp);
