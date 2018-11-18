8a63963dc4411047edaffc75b5f1a05d6b9bc499
tracer-ics2018 <tracer@njuics.org>
2018-11-18 14:52:53 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  14:52:53 up 21:30,  9 users,  load average: 0.20, 0.17, 0.17 f5d7094d218631276a9e07807e6d8c9430b371e7

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -42,8 +42,8 @@ int fs_open(const char *pathname,int flags,int mode){
  	for(int i=0;i<NR_FILES;i++){
 		if(strcmp(file_table[i].name,pathname)==0)
 		{
-			return i;
 			Log("fd:%d",i);
+			return i;
 		}
 	}
 	assert(0);
