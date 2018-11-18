e9d5b8d47dc1a82715716656871c71e810ecc3be
tracer-ics2018 <tracer@njuics.org>
2018-11-18 13:29:19 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  13:29:19 up 20:07,  7 users,  load average: 0.06, 0.15, 0.11 9db22ede8fc552986fcf2eb6ef4db46918eb5f9a

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,11 +1,15 @@
 #include "proc.h"
-
+#include <unistd.h>
 #define DEFAULT_ENTRY 0x4000000
 
 size_t get_ramdisk_size();
 size_t ramdisk_write(const void *buf,size_t offset,size_t len);
 size_t ramdisk_read(void *buf,size_t offset,size_t len);
 extern uint8_t ramdisk_start;
+extern int fs_open(const char *pathname,int flags,int mode);
+extern ssize_t fs_read(int fd,void *buf,size_t len);
+extern int fs_close(int fd);
+extern int fs_filesz(int fd);
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
   //void *bufff=NULL;
@@ -13,9 +17,13 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //ramdisk_read(bufff,0,len);
   //ramdisk_write(buf,0x4000000,len);
   //memcpy(&ramdisk_start+0x3EFEDA8,bufff,len);
-  size_t len=get_ramdisk_size();
-  void *bufff=(void *)0x4000000;
-  ramdisk_read(bufff,0,len);
+  //below
+  //size_t len=get_ramdisk_size();
+  //void *bufff=(void *)0x4000000;
+  //ramdisk_read(bufff,0,len);
+  int fd = fs_open(filename,0,0);
+  fs_read(fd,(void *)DEFAULT_ENTRY,fs_filesz(fd));
+  fs_close(fd);
   return DEFAULT_ENTRY;
 }
 
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,16 @@
 #include "common.h"
 #include "syscall.h"
+#include <unistd.h>
 
 void _halt(int code);
 int sys_yield();
 int sys_write(_Context *c);
 int sys_brk(_Context *c);
-
+extern int fs_open(const char *pathname, int flags,int mode);
+extern ssize_t fs_read(int fd,void *buf,size_t len);
+extern ssize_t fs_write(int fd,const void *buf,size_t len);
+extern off_t fs_lseek(int fd,off_t offset,int whence);
+extern int fs_close(int fd);
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -13,11 +18,15 @@ _Context* do_syscall(_Context *c) {
   a[2] = c->GPR3;
   a[3] = c->GPR4;
   //printf("at_syscall.c ebx %d\n",c->GPR2);
-  switch (a[0]) {
-	case 0: _halt(a[1]); break;
-	case 1: c->GPRx=sys_yield(); break;
-	case 4: c->GPRx=sys_write(c); break;
-    case 9: c->GPRx=sys_brk(c); break;
+  switch (a[0] ) {
+	case SYS_exit: _halt(a[1]); break;
+	case SYS_yield: c->GPRx=sys_yield(); break;
+	case SYS_open: c->GPRx=fs_open((const char *)a[1],a[2],a[3]); break;
+	case SYS_read: c->GPRx=fs_read(a[1],(void *)a[2],a[3]);break;
+	case SYS_write: c->GPRx=fs_write(a[1],(void *)a[2],a[3]); break;
+	case SYS_close: c->GPRx=fs_close(a[1]);break;
+	case SYS_lseek: c->GPRx=fs_lseek(a[1],a[2],a[3]);break;
+    case SYS_brk: c->GPRx=sys_brk(c); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   } 
 
@@ -34,6 +43,7 @@ int sys_exit(int code){
 	return 0;
 }
 */
+/*
 int sys_write(_Context *c){
 	assert(c->GPR2<=2);
 	char *buf=(char *)c->GPR3;
@@ -41,7 +51,7 @@ int sys_write(_Context *c){
 		_putc(buf[i]);
 	return SYS_write; 
 }
-
+*/
 int sys_brk(_Context *c){
 	return 0;
 }
