aff31134406cc7d77900cc4e15a2b615b7beb794
tracer-ics2018 <tracer@njuics.org>
2018-11-18 13:33:56 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  13:33:56 up 20:11,  8 users,  load average: 0.20, 0.19, 0.13 83cfce133bd1a1503948c681c6e6bba540daa013

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -81,7 +81,7 @@ ssize_t fs_write(int fd,const void *buf,size_t len){
 	return 0;
 }
 
-off_t lseek(int fd,off_t offset,int whence){
+off_t fs_lseek(int fd,off_t offset,int whence){
 	switch(whence){
 		case SEEK_SET: 
 			if(offset<0 || offset>file_table[fd].size)
@@ -103,7 +103,7 @@ off_t lseek(int fd,off_t offset,int whence){
 			return 0;
  	}
 }
-int close(int fd){
+int fs_close(int fd){
 	return 0;
 }
 
