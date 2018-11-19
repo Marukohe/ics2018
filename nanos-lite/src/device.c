fcc54cb1d23536ff92bd0411b9ba0080da011392
tracer-ics2018 <tracer@njuics.org>
2018-11-19 22:16:31 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  22:16:31 up 1 day, 19:06,  8 users,  load average: 0.42, 0.20, 0.18 9bcef494e60639d77ee06cda15355358e66a5c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -21,13 +21,13 @@ size_t events_read(void *buf, size_t offset, size_t len) {
   if(key!=_KEY_NONE){
 	if((key&0x8000)==0)  //down
 	{
-		assert(0);
+		//assert(0);
 		//sprintf(buf,"ku %s\n",keyname[key]);
 		snprintf(buf,len,"ku %s\n",keyname[key]);
 	}
 	else{    //code
 		//sprintf(buf,"kd %s\n",keyname[key&0x7fff]);
-		assert(0);
+		//assert(0);
 		snprintf(buf,len,"kd %s\n",keyname[key&0x7fff]);
 	}
   }
