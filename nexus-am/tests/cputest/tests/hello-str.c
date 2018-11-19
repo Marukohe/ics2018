9cb09a7ed20aa78d22d0e345ce2f40bd48eae6bb
tracer-ics2018 <tracer@njuics.org>
2018-11-19 23:52:51 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  23:52:51 up 1 day, 20:43, 15 users,  load average: 0.13, 0.14, 0.11 e791b6945535d796e00319fb171a922145005cb4

--- nexus-am/tests/cputest/tests/hello-str.c
+++ nexus-am/tests/cputest/tests/hello-str.c
@@ -14,10 +14,10 @@ int main() {
 	//printf("%s",buf);
 	nemu_assert(strcmp(buf, "2 + 10 = 12\n") == 0);
 
-	//char buff[256];
-	snprintf(buf,5,"%d + %d = %d\n",2,10,12);
-	printf("%s\n",buf);
-	nemu_assert(strcmp(buf,"2 + 1 \n")==0);
+	char buff[256];
+	snprintf(buff,5,"%d + %d = %d\n",2,10,12);
+	printf("%s\n",buff);
+	nemu_assert(strcmp(buff,"2 + \n")==0);
 	//nemu_assert(strcmp("2d + 10d = 12d","2 + 10 = 12")==0);
 	//int a=10;
 	//char *s="hello,world";
