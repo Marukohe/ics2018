10023c674db175d41838e4f9513a96a2db936904
tracer-ics2018 <tracer@njuics.org>
2018-11-20 22:58:12 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  22:58:12 up 2 days,  1:48,  5 users,  load average: 0.24, 0.32, 0.38 9b7fe260b7b363e1a8f56575aa554df119208049

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -121,7 +121,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
   int row,col;
   offset /= 4;
   col = offset % screen_width();
-  row = offset / screen_height();
+  row = offset / screen_width();
   draw_rect((uint32_t*)buf, col, row, len/4, 1);
   
   return len;
