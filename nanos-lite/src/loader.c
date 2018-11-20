9ac37f9cd3e87548898763774c4a56647345b77e
tracer-ics2018 <tracer@njuics.org>
2018-11-20 23:07:54 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  23:07:54 up 2 days,  1:58,  3 users,  load average: 1.06, 1.05, 0.74 bde572215a78eaeb7768845a72ccc12b235e85b

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,40 +1,35 @@
 #include "proc.h"
-#include "common.h"
-
+#include <unistd.h>
 #define DEFAULT_ENTRY 0x4000000
 
-//#define BUFF_SIZE 1024
-//char buff[BUFF_SIZE];
-
 size_t get_ramdisk_size();
-size_t ramdisk_read(void *buf, size_t offset, size_t len);
-size_t fs_filesz(int fd);
-int fs_open(const char *pathname, int flags, int mode);
-size_t fs_read(int fd, void *buf, size_t len);
+size_t ramdisk_write(const void *buf,size_t offset,size_t len);
+size_t ramdisk_read(void *buf,size_t offset,size_t len);
+extern uint8_t ramdisk_start;
+extern int fs_open(const char *pathname,int flags,int mode);
+extern ssize_t fs_read(int fd,void *buf,size_t len);
+extern int fs_close(int fd);
+extern int fs_filesz(int fd);
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
-  int NO = fs_open(filename, 0, 0);
-  fs_read(NO, (void*)DEFAULT_ENTRY, fs_filesz(NO));
-  //printf("loader: I'm here.\n");
-  //ramdisk_read((void*)DEFAULT_ENTRY, 0, get_ramdisk_size());
-  //printf("lodder: get_ramdisk_size():%#x\n", get_ramdisk_size());
-  /*
-  size_t s = get_ramdisk_size();  
-  int i;
-  for(i = 0;(i+1)*BUFF_SIZE <= s;i++){
-    ramdisk_read(buff, i*BUFF_SIZE, BUFF_SIZE);
-    memcpy((void*)(DEFAULT_ENTRY+i*BUFF_SIZE), buff, BUFF_SIZE);
-  }
-  ramdisk_read(buff, i*BUFF_SIZE, s-BUFF_SIZE*i);
-  memcpy((void*)(DEFAULT_ENTRY+i*BUFF_SIZE), buff, s-BUFF_SIZE*i);
-  */
+  //void *bufff=NULL;
+  //size_t len = get_ramdisk_size();
+  //ramdisk_read(bufff,0,len);
+  //ramdisk_write(buf,0x4000000,len);
+  //memcpy(&ramdisk_start+0x3EFEDA8,bufff,len);
+  //below
+  //size_t len=get_ramdisk_size();
+  //void *bufff=(void *)0x4000000;
+  //ramdisk_read(bufff,0,len);
+  int fd = fs_open(filename,0,0);
+  fs_read(fd,(void *)DEFAULT_ENTRY,fs_filesz(fd));
+  fs_close(fd);
   return DEFAULT_ENTRY;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
   ((void(*)())entry) ();
-  //printf("naive_uload: I'm here\n");
 }
 
 void context_kload(PCB *pcb, void *entry) {
