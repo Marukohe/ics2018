94f64296bf805188327bb853880685233ae715e4
tracer-ics2018 <tracer@njuics.org>
2018-11-20 23:39:37 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  23:39:37 up 2 days,  2:26,  1 user,  load average: 0.28, 0.18, 0.26 7ae5fbaf45d71c9b8d641be4852c4299eb954e9

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -2,19 +2,19 @@
 /*
 make_EHelper(add) {
   
+  t3 = id_dest->val;
   rtl_add(&t2, &id_dest->val, &id_src->val);
-  rtl_setrelop(RELOP_LTU, &t3, &t2, &id_dest->val);
   
   operand_write(id_dest, &t2);
   rtl_update_ZFSF(&id_dest->val,id_dest->width);
 
-  rtl_setrelop(RELOP_LTU, &t0, &t2, &id_dest->val);
+  rtl_setrelop(RELOP_LTU, &t0, &id_dest->val, &t3);
   rtl_set_CF(&t0);
 
-  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t0, &t3, &id_src->val);
   rtl_not(&t0, &t0);
-  rtl_xor(&t1, &id_dest->val, &t2);
-  rtl_and(&t0, &t0, &t1);
+  rtl_xor(&t1, &id_dest->val, &id_dest->val);
+  rtl_and(&t0, &t0, &t3);
   rtl_msb(&t0, &t0, id_dest->width);
   rtl_set_OF(&t0);
 
