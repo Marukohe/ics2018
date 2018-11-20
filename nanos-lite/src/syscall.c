b096820fa2c6cb1878005c44a26ae777b2e2a56a
tracer-ics2018 <tracer@njuics.org>
2018-11-20 21:06:37 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  21:06:37 up 1 day, 23:57,  1 user,  load average: 1.43, 0.87, 0.64 b26298d76c0326e661f7381bf503c898b6d243

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,57 +1,96 @@
 #include "common.h"
 #include "syscall.h"
-#include <unistd.h>
+#include "fs.h"
 
-void _halt(int code);
-int sys_yield();
-int sys_write(_Context *c);
-int sys_brk(_Context *c);
-extern int fs_open(const char *pathname, int flags,int mode);
-extern ssize_t fs_read(int fd,void *buf,size_t len);
-extern ssize_t fs_write(int fd,const void *buf,size_t len);
-extern off_t fs_lseek(int fd,off_t offset,int whence);
-extern int fs_close(int fd);
+char* NAME_OF_TASK[] = {
+  "SYS_exit",
+  "SYS_yield",
+  "SYS_open",
+  "SYS_read",
+  "SYS_write",
+  "SYS_kill",
+  "SYS_getpid",
+  "SYS_close",
+  "SYS_lseek",
+  "SYS_brk",
+  "SYS_fstat",
+  "SYS_time",
+  "SYS_signal",
+  "SYS_execve",
+  "SYS_fork",
+  "SYS_link",
+  "SYS_unlink",
+  "SYS_wait",
+  "SYS_times",
+  "SYS_gettimeofday"
+};
+
+intptr_t program_break;
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-  //printf("at_syscall.c ebx %d\n",c->GPR2);
-  switch (a[0] ) {
-	case SYS_exit: _halt(a[1]); break;
-	case SYS_yield: c->GPRx=sys_yield(); break;
-	case SYS_open: c->GPRx=fs_open((const char *)a[1],a[2],a[3]); break;
-	case SYS_read: c->GPRx=fs_read(a[1],(void *)a[2],a[3]);break;
-	case SYS_write: c->GPRx=fs_write(a[1],(void *)a[2],a[3]); break;
-	case SYS_close: c->GPRx=fs_close(a[1]);break;
-	case SYS_lseek: c->GPRx=fs_lseek(a[1],a[2],a[3]);break;
-    case SYS_brk: c->GPRx=sys_brk(c); break;
+  //printf("do_syscall: I'm here.\n");
+  //printf("do_syscall: a:%s %d %d %d\n", NAME_OF_TASK[a[0]], a[1], a[2], a[3]);
+
+  /*
+  if(program_break == -1){
+    //printf("do_syscall: Enter break.\n");
+    extern intptr_t end;
+    program_break = end;
+  }
+  */
+
+  //printf("do_syscall: I'm here.\n");
+
+  switch (a[0]) {
+    case SYS_exit: _halt(a[1]); return c;
+    case SYS_yield: _yield(); c->GPR1 = 0; return c;
+    case SYS_open:
+      c->GPR1 = fs_open((char*)a[1], (int)a[2], (int)a[3]);
+      return c;
+    case SYS_read:
+      c->GPR1 = fs_read((int)a[1], (void*)a[2], (size_t)a[3]);
+      return c;
+    case SYS_write:
+      c->GPR1 = fs_write((int)a[1], (void*)a[2], (size_t)a[3]);
+      //printf("do_syscall: SYS_write:%#x\n", c->GPR1);
+      return c;
+      /*
+      if(a[1] == 1 || a[1] == 2){
+        for(i = 0;i < a[3];i++){
+          _putc(((char*)a[2])[i]);       
+        }
+        c->GPR1 = i;
+        //a[0] = a[3];
+        //printf("do_syscall: SYS_write:1:%d\n", i);
+      }
+      else{
+        c->GPR1 = fs_write((int)a[1], (void*)a[2], (size_t)a[3]);
+      }
+      return c;
+      */
+    case SYS_close:
+      c->GPR1 = fs_close((int)a[1]);
+      return c;
+    case SYS_lseek:
+      c->GPR1 = fs_lseek((int)a[1], (size_t)a[2], (int)a[3]);
+      //printf("SYS_lseek: %d\n", c->GPR1);
+      return c;
+    case SYS_brk:
+      program_break = a[1];
+      c->GPR1 = 0;
+      //a[0] = 0;
+      /*
+      end = a[1];
+      a[0] = 0;
+      */
+      //panic("hhh\n");
+      return c;
     default: panic("Unhandled syscall ID = %d", a[0]);
-  } 
+  }
 
   return NULL;
-}
-
-int sys_yield(){
-	_yield();
-	return 0;
-}
-/*
-int sys_exit(int code){
-	_halt(code);
-	return 0;
-}
-*/
-/*
-int sys_write(_Context *c){
-	assert(c->GPR2<=2);
-	char *buf=(char *)c->GPR3;
-	for(int i=0;i<c->GPR4;i++)
-		_putc(buf[i]);
-	return SYS_write; 
-}
-*/
-int sys_brk(_Context *c){
-	return 0;
 }
