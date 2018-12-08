e86f08cbb605cb12b703a59626129934ed242742
tracer-ics2018 <tracer@njuics.org>
2018-12-08 23:41:05 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  23:41:05 up  4:28,  3 users,  load average: 0.47, 0.24, 0.64 d87d750b16c125817f9683c64c1763da4d70a19

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -37,7 +37,7 @@ void context_kload(PCB *pcb, void *entry) {
   stack.start = pcb->stack;
   stack.end = stack.start + sizeof(pcb->stack);
 
-  pcb->tf = _kcontext(stack, entry, NULL);
+  pcb->cp = _kcontext(stack, entry, NULL);
 }
 
 void context_uload(PCB *pcb, const char *filename) {
@@ -47,5 +47,5 @@ void context_uload(PCB *pcb, const char *filename) {
   stack.start = pcb->stack;
   stack.end = stack.start + sizeof(pcb->stack);
 
-  pcb->tf = _ucontext(&pcb->as, stack, stack, (void *)entry, NULL);
+  pcb->cp = _ucontext(&pcb->as, stack, stack, (void *)entry, NULL);
 }
