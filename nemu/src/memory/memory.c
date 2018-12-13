ef553feda4516d80563138ce9b818a08a2484ec2
tracer-ics2018 <tracer@njuics.org>
2018-12-13 21:08:29 +0800

    >  compile 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  21:08:29 up  3:26,  8 users,  load average: 0.03, 0.02, 0.03 cdb635d2a1689507fd1885bd3d0f1d63922bb8c8

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -34,6 +34,7 @@ paddr_t page_translate(vaddr_t addr){
 	PTE *pagte,pte;
 	paddr_t paddr = addr;
 	if(cpu.cr0.paging){
+		assert(0);
 		pagde = (PDE*)(uintptr_t)(cpu.cr3.page_directory_base<<12);
 		pde.val = paddr_read((uintptr_t)&pagde[addr>>22],4);
 		assert(pde.present);
