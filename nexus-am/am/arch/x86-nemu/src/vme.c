821b4a601c771396eb021a3d4b36ff3053194c78
tracer-ics2018 <tracer@njuics.org>
2018-12-14 20:45:40 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  20:45:40 up 11:34, 10 users,  load average: 0.08, 0.10, 0.06 cef60b235fd3c4a16089ada5c487504eb29751a8

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -87,10 +87,10 @@ int _map(_Protect *p, void *va, void *pa, int mode) {
 	if(!(uint32_t)pagde&PTE_P){   //new page
 		//assert(0);
 		pte = (PTE*)(pgalloc_usr(1));
-		assert(0);
+		//assert(0);
 		//*pagde = PTE_ADDR(pte) | PTE_P;
-		*pagde = ((uint32_t)(pte)&~0xfff) | PTE_P;
-		assert(0);
+		//*pagde = ((uint32_t)(pte)&~0xfff) | PTE_P;
+		//assert(0);
 	}
 	else{
 		pte = (PTE*)PTE_ADDR(*pagde);
