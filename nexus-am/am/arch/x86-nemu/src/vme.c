a4ef0f3d0c25999897379b32eda90cdcb066c0a8
tracer-ics2018 <tracer@njuics.org>
2018-12-14 21:26:38 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  21:26:38 up 12:15, 10 users,  load average: 0.04, 0.34, 0.30 939a071026cf58048824d3f8ad729fd671c9717c

--- nexus-am/am/arch/x86-nemu/src/vme.c
+++ nexus-am/am/arch/x86-nemu/src/vme.c
@@ -90,9 +90,9 @@ int _map(_Protect *p, void *va, void *pa, int mode) {
 		//assert(0);
 		for(int i=0;i<NR_PTE;i++)
 			pte[i]=0;
-		//*pagde = PTE_ADDR(pte) | PTE_P;
+		*pagde = PTE_ADDR(pte) | PTE_P;
 		//*pagde = ((uint32_t)(pte)&~0xfff) | PTE_P;
-		((uint32_t *)(p->ptr))[PDX(va)] = PTE_ADDR(pte) | PTE_P;
+		//((uint32_t *)(p->ptr))[PDX(va)] = PTE_ADDR(pte) | PTE_P;
 		//assert(0);
 	}
 	else{
