e1a7fc73ad2c73979fbc8d5012d34c79b6a8526a
tracer-ics2018 <tracer@njuics.org>
2018-12-15 14:02:01 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  14:02:01 up 19:02,  9 users,  load average: 0.54, 0.27, 0.60 6941d5465cac36fb903685349c6639129f324a2e

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -41,12 +41,12 @@ _Context* irq_handle(_Context *tf) {
       next = tf;
     }
   }
-  printf("cte next: %x\n",(uint32_t)next->prot->ptr);
-  printf("cte tf: %x\n",(uint32_t)tf->prot->ptr);
-  printf("at irq before switch\n");
+  //printf("cte next: %x\n",(uint32_t)next->prot->ptr);
+  //printf("cte tf: %x\n",(uint32_t)tf->prot->ptr);
+  //printf("at irq before switch\n");
   _switch(next);
 
-  printf("at irq after switch\n");
+  //printf("at irq after switch\n");
   return next;
 }
 
