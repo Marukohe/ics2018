dcb4e914eed99b971b88dd951750531f8fa75a66
tracer-ics2018 <tracer@njuics.org>
2018-12-15 14:27:28 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  14:27:28 up 19:27, 11 users,  load average: 0.16, 0.14, 0.23 d3fde2e05d60467c420c54bd44ae9c18632f260f

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -81,7 +81,7 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 */
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-//	_yield();
+	_yield();
   
   //Log("offset %d",offset);
   int row,col;
