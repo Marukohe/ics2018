def557069ffdb3c9406054b2488985e993c65d53
tracer-ics2018 <tracer@njuics.org>
2018-12-15 17:42:30 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  17:42:30 up 22:42, 12 users,  load average: 0.19, 0.33, 0.32 20b116d8662e8a5697e9433ff659f9d320c8349a

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,8 +30,8 @@ void init_proc() {
 	
 	//context_kload(&pcb[0],(void *)hello_fun);
 	//_protect(&pcb[0].as);
-	context_uload(&pcb[0],"/bin/pal");
-	//context_uload(&pcb[1],"/bin/hello");
+	context_uload(&pcb[0],"/bin/hello");
+	context_uload(&pcb[1],"/bin/pal");
 
 	//Log("pcb ptr %x",(uintptr_t)((&pcb[0])->as.ptr));
 /*	Log("pcb edi %x",(&pcb[0])->cp->edi);
@@ -58,8 +58,8 @@ _Context* schedule(_Context *prev) {
 	//Log("eip %x",current->cp->eip);
 
 	//always select pcb[0] as the new process
-	//current = (current == &pcb[0]?&pcb[1]:&pcb[0]);
-	current = &pcb[0];
+	current = (current == &pcb[0]?&pcb[1]:&pcb[0]);
+	//current = &pcb[0];
 //	Log("%x",(uintptr_t)current->as.ptr);
 
 	//then return the new context
