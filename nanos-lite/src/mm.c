d298073ae52022776b45526d7c5868ff01a6bc7f
tracer-ics2018 <tracer@njuics.org>
2018-12-15 14:22:17 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  14:22:17 up 19:22, 10 users,  load average: 0.12, 0.14, 0.26 51be3ef623c161a6b55e2fe83e5d239547ac06fb

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -17,7 +17,7 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t new_brk) {
-	Log("cur_brk: %x\nmax_brk: %x\nnew_brk: %x",current->cur_brk,current->max_brk,new_brk);
+//	Log("cur_brk: %x\nmax_brk: %x\nnew_brk: %x",current->cur_brk,current->max_brk,new_brk);
 	if(current->cur_brk == 0){
 		current->cur_brk = current->max_brk = new_brk;
 	}
@@ -29,14 +29,14 @@ int mm_brk(uintptr_t new_brk) {
 			uintptr_t begin = (current->max_brk)&~0xfff;
 			for(va = begin;va<=new_brk;va+=PGSIZE){
 				pa = new_page(1);
-				Log("pa: %x",(uintptr_t)pa);
+				//Log("pa: %x",(uintptr_t)pa);
 				_map(&current->as,(void*)va,pa,1);
 			}
 			current->max_brk = va;
 		}
 		current->cur_brk = new_brk;
 	}
-	Log("cur_brk: %x   max_brk: %x\n",current->cur_brk,current->max_brk);
+	//Log("cur_brk: %x   max_brk: %x\n",current->cur_brk,current->max_brk);
   return 0;
 }
 
