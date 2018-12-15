91c9f7d0215bf32daf74d6e34166a48d09a544cd
tracer-ics2018 <tracer@njuics.org>
2018-12-15 17:36:52 +0800

    >  run 171240537 hw Linux debian 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  17:36:52 up 22:37, 12 users,  load average: 0.62, 0.35, 0.30 74a7f3f371992ded7cebda2f6e8237c852b515a2

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -59,7 +59,7 @@ _Context* schedule(_Context *prev) {
 
 	//always select pcb[0] as the new process
 	//current = (current == &pcb[0]?&pcb[1]:&pcb[0]);
-	//current = &pcb[0];
+	current = &pcb[0];
 //	Log("%x",(uintptr_t)current->as.ptr);
 
 	//then return the new context
